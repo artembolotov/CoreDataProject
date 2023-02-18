af05edac9cce95b0a085a42e9dbf15b3ffc5f74e
Artem Bolotov <teeemon@gmail.com>
2023-02-18 08:20:42 +0300

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by artembolotov on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
